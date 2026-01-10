ee225210e3d6469f45005a5812cc3fb4a3f504fa
AdeMBCH <153865655+AdeMBCH@users.noreply.github.com>
2026-01-10 15:43:34 +0100

    build.sh bug maj

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 BUILD_DIR="${BUILD_DIR:-build}"
-BUILD_TYPE="${BUILD_TYPE:-Release}"
+BUILD_TYPE="${BUILD_TYPE:-Debug}"
 
 cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 cmake --build "$BUILD_DIR" -j
